486165902876420739cf070a46a08550a48b9a5d
nathanjekel <ijekel2@gmail.com>
2018-06-13 07:44:04 -0400

    Server responds with HelloWorld

--- MyHttpServer/MyHttpServer/src/com/nathanjekel/java/server/MyHttpServer.java
+++ MyHttpServer/MyHttpServer/src/com/nathanjekel/java/server/MyHttpServer.java
@@ -1,11 +1,42 @@
 package com.nathanjekel.java.server;
 
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class MyHttpServer {
 
+	//
+	// Argument 1: Host
+	// Argument 2: Port
 	public static void main(String[] args) {
-		
+		String host = args[0];
+		Integer port = new Integer(args[1]);
+		try(ServerSocket serverSocket = new ServerSocket()) {
+			InetSocketAddress socketAddress = new InetSocketAddress(host, port);
+			serverSocket.bind(socketAddress);
+			System.out.println("Serving HTTP on port " + port + "...");
+			while(true) {
+				Socket client = serverSocket.accept();
+				InputStreamReader inputStreamReader = new InputStreamReader(client.getInputStream());
+				BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+				
+				String inputLine = bufferedReader.readLine(); 
+				while (!inputLine.isEmpty()) { 
+					System.out.println(inputLine); 
+					inputLine = bufferedReader.readLine();
+				}
+
+			    String response = "HTTP/1.1 200 OK\n\n\n Hello, World!";
+			    client.getOutputStream().write(response.getBytes());
+			    client.close();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
 	}
 
