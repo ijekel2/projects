408b6a9d841cd1cd30361fd9a84f394659a13fee
nathanjekel <ijekel2@gmail.com>
2018-06-15 08:25:05 -0400

    refactoring

--- MyHttpServer/MyHttpServer/src/com/nathanjekel/java/server/MyHttpServer.java
+++ MyHttpServer/MyHttpServer/src/com/nathanjekel/java/server/MyHttpServer.java
@@ -15,29 +15,43 @@ public class MyHttpServer {
 	public static void main(String[] args) {
 		String host = args[0];
 		Integer port = new Integer(args[1]);
+		
 		try(ServerSocket serverSocket = new ServerSocket()) {
-			InetSocketAddress socketAddress = new InetSocketAddress(host, port);
-			serverSocket.bind(socketAddress);
-			System.out.println("Serving HTTP on port " + port + "...");
+			bindToClientSocket(serverSocket, host, port);
+			
 			while(true) {
 				Socket client = serverSocket.accept();
-				InputStreamReader inputStreamReader = new InputStreamReader(client.getInputStream());
-				BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-				
-				String inputLine = bufferedReader.readLine(); 
-				while (!inputLine.isEmpty()) { 
-					System.out.println(inputLine); 
-					inputLine = bufferedReader.readLine();
-				}
-
-			    String response = "HTTP/1.1 200 OK\n\n\n Hello, World!";
-			    client.getOutputStream().write(response.getBytes());
-			    client.close();
+				readRequest(client);
+				sendResponse(client);
 			}
+			
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 
+	}
+	
+	private static void bindToClientSocket(ServerSocket serverSocket, String host, int port) throws IOException {
+		InetSocketAddress socketAddress = new InetSocketAddress(host, port);
+		serverSocket.bind(socketAddress);
+		System.out.println("Serving HTTP on port " + port + "...");
+	}
+	
+	private static void readRequest(Socket client) throws IOException {
+		InputStreamReader inputStreamReader = new InputStreamReader(client.getInputStream());
+		BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+		String inputLine = bufferedReader.readLine(); 
+		
+		while (!inputLine.isEmpty()) { 
+			System.out.println(inputLine); 
+			inputLine = bufferedReader.readLine();
+		}
+	}
+	
+	private static void sendResponse(Socket client) throws IOException {
+	    String response = "HTTP/1.1 200 OK\n\n Hello, World!";
+	    client.getOutputStream().write(response.getBytes());
+	    client.close();
 	}
 
 }
